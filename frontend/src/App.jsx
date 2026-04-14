f2dcd4ea22760e50a6e9cfb36c007f9853d684d2
ShanmugaPriyan0603 <ShanmugaPriyan0603@users.noreply.github.com>
2026-04-14 19:52:06 +0530

    Changes in Inventory

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import BatchDetails from './pages/BatchDetails';
 import Verify from './pages/Verify';
+import Inventory from './pages/Inventory';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/inventory" element={<Inventory />} />
       <Route path="/batch/:batchId" element={<BatchDetails />} />
       <Route path="/verify" element={<Verify />} />
     </Routes>
